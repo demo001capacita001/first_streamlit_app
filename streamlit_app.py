051a44b8e1c0b47fc1907717babaf04da414efb9
demo001capacita001 <123007962+demo001capacita001@users.noreply.github.com>
2023-01-18 11:49:15 -0600

    Update streamlit_app.py
    Added 2 more options for the menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.title('Breakfast Menu')
 
 streamlit.title('Omega 3 & Bluberry Oatmeal')
+
+  streamlit.title('Kale, Spinach & Rocket Smoothie')
+
+  streamlit.title('Hard-Boiled Free-Range Egg')
