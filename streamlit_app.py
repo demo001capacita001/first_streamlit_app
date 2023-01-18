e4af998b1b653d397819739123a57d853a855271
demo001capacita001 <123007962+demo001capacita001@users.noreply.github.com>
2023-01-18 12:09:27 -0600

    Update streamlit_app.py
    Added new title "Build Your Own Fruit Smoothie"

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,6 @@ streamlit.text('🥗🥤 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥚 Hard-Boiled Free-Range Egg')
 
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.title('🍌🥭  Build Your Own Fruit Smoothie  🥝🍇')
+
