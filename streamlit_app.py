aed663dd6e6819d83dd1b5db3c60b68dcf60e3c9
demo001capacita001 <123007962+demo001capacita001@users.noreply.github.com>
2023-01-18 12:05:16 -0600

    Update streamlit_app.py
    Added new option ("Avocado Toast"), changed title and header, added emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,13 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
-streamlit.text('Omega 3 & Bluberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Bluberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗🥤 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥚 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
