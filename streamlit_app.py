4f99416735e5eafc3fcd822c5c97856f1c047357
demo001capacita001 <123007962+demo001capacita001@users.noreply.github.com>
2023-01-24 18:10:06 -0600

    Update streamlit_app.py
    Added -->
    streamlit.header("The fruit load list contains: ")
    # Snoflake-related functions
    def get_fruit_load_list():
         with my_cnx.cursor() as my_cur:
         my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
         return my_cur.fetchall()
    
    # Add a button to load the fruit
    if streamlit.button('Get fruit from list'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         my_data_rows = get_fruit_load_list()
         streamlit.dataframe(my_data_rows)

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,6 @@ try:
 except URLError as e:
      streamlit.error()
        
-
 #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_cur = my_cnx.cursor()
 #my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -53,14 +52,19 @@ except URLError as e:
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-#my_data_row = my_cur.fetchone()    #extrae un solo registro
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-#streamlit.text(my_data_row)  "arroja una línea o renglón sencillos
-streamlit.dataframe(my_data_rows)  #para mejorar la visibilidad de los resultados
+
+streamlit.header("The fruit load list contains: ")
+# Snoflake-related functions
+def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur:
+     my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+     return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get fruit from list'):
+     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+     my_data_rows = get_fruit_load_list()
+     streamlit.dataframe(my_data_rows)  
 
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
